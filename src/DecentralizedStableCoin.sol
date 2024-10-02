9ab40cc6e53154532909417e7eef4a4a2b028c3b
Divine <officialkb17@gmail.com>
2024-10-02 15:54:52 +0100

    dsc contract init

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity 0.8.19;
+
+/*
+ * @title DecentralizedStableCoin
+ * @author Divine Charles
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+* This is the contract meant to be governed by DSCEngine. It is an ERC20 token that can be minted and burned by the
+DSCEngine smart contract.
+ */
+
+contract DecentralizedStableCoin {
+
+}
