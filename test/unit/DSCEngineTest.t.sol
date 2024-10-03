e45a6542a301a01ef8f2957f06af05b0ecace182
Divine <officialkb17@gmail.com>
2024-10-03 14:11:57 +0100

    set up DSCEngine test

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine dsce;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dsce) = deployer.run();
+    }
+}
