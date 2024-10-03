4c4710d3a5346faca1642f7f99e6a8f354fc4c7f
Divine <officialkb17@gmail.com>
2024-10-03 13:01:16 +0100

    complete mint function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -59,6 +59,7 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__TokenAddressesAndPricefeedAddressesMustBeTheSameLength();
     error DSCEngine__TransferFailed();
     error DSCEngine__BreaksHealthFactor(uint256 healthFactorValue);
+    error DSCEngine__MintFailed();
 
     ///////////////////
     // State Variables
@@ -153,6 +154,10 @@ contract DSCEngine is ReentrancyGuard {
     function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
         s_DSCMinted[msg.sender] += amountDscToMint;
         _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if (!minted) {
+            revert DSCEngine__MintFailed();
+        }
     }
 
     function burnDsc() external {}
