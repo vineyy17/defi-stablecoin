3ebd83bc25b305e4f76b8680f6820d4cb47e9305
Divine <officialkb17@gmail.com>
2024-10-04 15:58:03 +0100

    start writing depositCollateral tests

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,14 +6,49 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
     DecentralizedStableCoin dsc;
     DSCEngine dsce;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
+
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
     function setUp() public {
         deployer = new DeployDSC();
-        (dsc, dsce) = deployer.run();
+        (dsc, dsce, config) = deployer.run();
+        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
+
+    //////////////////
+    // Price Tests //
+    /////////////////
+    function testGetUsdValue() public {
+        uint256 ethAmount = 15e18;
+        // 15e18 ETH * $2000/ETH = $30,000e18
+        uint256 expectedUsd = 30_000e18;
+        uint256 usdValue = dsce.getUsdValue(weth, ethAmount);
+        assertEq(usdValue, expectedUsd);
+    }
+
+    ///////////////////////////////
+    // depositCollateral Tests //
+    /////////////////////////////
+
+    function testRevertsIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }
